781afbcbbd8f1ace959387e2e489a34d60ed8717
Colin Chan <colin.th.chan@gmail.com>
2020-09-03 12:05:34 -0400

    Add mouse position hook and move canttouchthis component with mouse position

--- containers/app/src/components/CantTouchThis/CantTouchThis.js
+++ containers/app/src/components/CantTouchThis/CantTouchThis.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import UseMousePosition from '../../hooks/GetMousePosition';
 
-const cantTouchThis = () => {
-  return <p>Can't touch this! ... na nana na... Can't touch this!</p>
-};
+function CantTouchThis() {
+  const { x, y } = UseMousePosition();
+  const hasMovedCursor = typeof x === "number" && typeof y === "number";
+  
+  const childRef = useRef(0);
+  //const [width, setWidth] = useState(0)
+  //const [height, setHeight] = useState(0)
+  //useEffect(() => {
+    //if(childRef.current){
+      //setWidth(childRef.current.offsetWidth);
+      //setHeight(childRef.current.offsetHeight);
+    //};
 
-export default cantTouchThis;
+  //}, [childRef]);
+  //console.log(width, height);
+  console.log("useRef: ", childRef.current);
+  console.log("offsetWidth: ", childRef.current.offsetWidth, "offsetHeight: ", childRef.current.offsetHeight);
+  
+  //var offsetX = x - (width / 2);
+  //var offsetY = y - (height / 2);
+  var offsetX = x - (childRef.current.offsetWidth / 2);
+  var offsetY = y - (childRef.current.offsetHeight / 2);
+
+  console.log(offsetX, offsetY);
+
+  var movingStyle = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    border: "border-box",
+    padding: "50px",
+    transform: `translate3d(${offsetX}px, ${offsetY}px, 0)`
+  };
+
+  return (
+      <div style={movingStyle} ref={childRef}>
+        <p> { hasMovedCursor ? `Your cursor is at ${x}, ${y}.` : "Move your mouse around." } </p> 
+      </div>
+  );
+}
+
+export default CantTouchThis;
