90a6fbba125397d95fa06adf4822e13ead7f9df8
Colin Chan <colin.th.chan@gmail.com>
2020-09-09 18:24:03 -0400

    Add CantTouchThis component comments

--- containers/app/src/components/CantTouchThis/CantTouchThis.js
+++ containers/app/src/components/CantTouchThis/CantTouchThis.js
@@ -3,18 +3,20 @@ import UseMousePosition from '../../hooks/UseMousePosition';
 import './CantTouchThis.css';
 
 function CantTouchThis() {
+  // Get mouse position
   const { x, y } = UseMousePosition();
   const hasMovedCursor = typeof x === "number" && typeof y === "number";
   
+  // Create ref hook
   const childRef = useRef(0);
-
   console.log("offsetWidth: ", childRef.current.offsetWidth, "offsetHeight: ", childRef.current.offsetHeight);
   
+  // Set random element offset
   var offsetX = x - (Math.random() * 600);
   var offsetY = y - (Math.random() * 600);
-  
   console.log(offsetX, offsetY);
 
+  // Add variable element style
   var movingStyle = {
     transform: `translate3d(${offsetX}px, ${offsetY}px, 0)`,
   };
