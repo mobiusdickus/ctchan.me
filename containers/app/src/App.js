4eae44536e13c8634a7b3fc342e7506f23698cf7
Colin Chan <colin.th.chan@gmail.com>
2020-09-29 14:42:32 -0400

    Add react-router-dom lib

--- containers/app/src/App.js
+++ containers/app/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
@@ -28,3 +29,28 @@ function App() {
 }
 
 export default App;
+
+//import React from 'react';
+//import { Route, Switch } from 'react-router-dom';
+
+//import './App.css';
+//import Home from './components/Home/Home';
+//import About from './components/About/About';
+//import Contact from './components/Contact/Contact';
+//import CantTouchThis from './components/CantTouchThis/CantTouchThis';
+
+//function App() {
+
+  //return (
+    //<main> 
+      //<Switch>
+        //<Route exact path='/' component={Home}/>
+        //<Route exact path='/about' component={About}/>
+        //<Route exact path='/contact' component={Contact}/>
+        //<Route exact path='/canttouchthis' component={CantTouchThis}/>
+      //</Switch>
+    //</main>
+  //);
+//}
+
+//export default App;
